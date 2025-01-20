fb924d24ca2e21ada707402e65cc2c366cddb7ab
j-m-a-g <jmaghamez@gmail.com>
2025-01-19 21:42:31 -0500

    Updated Day3MullItOver.py

--- Other Problems/AdventOfCode2024/Day3MullItOver.py
+++ Other Problems/AdventOfCode2024/Day3MullItOver.py
@@ -11,53 +11,54 @@ day3Input = open(workingFile, "r")
 inputList = list(day3Input.readline().replace("\n", ""))
 multipliedIntegerProducts = []
 multipliedIntegers = 0
+
+firstInt = 0
+secondInt = 0
     
 for i in range(len(inputList)):
-    if inputList[i] == "m" and inputList[i + 1] == "u" and inputList[i + 2] == "l" and inputList[i + 3] == "(":
-        # X, Y
+    if inputList[i - 3] == "m" and inputList[i - 2] == "u" and inputList[i - 1] == "l" and inputList[i] == "(":
+        # X,Y
         if inputList[i + 5] == "," and inputList[i + 7] == ")":
-            multipliedIntegers = int(inputList[i + 4]) * int(inputList[i + 6])
-        # X, YY
+            firstInt = inputList[i + 4]
+            secondInt = inputList[i + 6]
+        # X,YY
         elif inputList[i + 5] == "," and inputList[i + 8] == ")":
+            firstInt = inputList[i + 4]
             secondInt = str(inputList[i + 6] + inputList[i + 7])
-            multipliedIntegers = int(inputList[i + 4]) * int(secondInt)
-        # X, YYY
+        # X,YYY
         elif inputList[i + 5] == "," and inputList[i + 9] == ")":
+            firstInt = inputList[i + 4]
             secondInt = str(inputList[i + 6] + inputList[i + 7] + inputList[i + 8])
-            multipliedIntegers = int(inputList[i + 4]) * int(secondInt)
-        # XX, Y
+        # XX,Y
         elif inputList[i + 6] == "," and inputList[i + 8] == ")":
             firstInt = str(inputList[i + 4] + inputList[i + 5])
-            multipliedIntegers = int(firstInt) * int(inputList[i + 7])
-        # XX, YY
+            secondInt = inputList[i + 7]
+        # XX,YY
         elif inputList[i + 6] == "," and inputList[i + 9] == ")":
             firstInt = str(inputList[i + 4] + inputList[i + 5])
             secondInt = str(inputList[i + 7] + inputList[i + 8])
-            multipliedIntegers = int(firstInt) * int(secondInt)
-        # XX, YYY
+        # XX,YYY
         elif inputList[i + 6] == "," and inputList[i + 10] == ")":
             firstInt = str(inputList[i + 4] + inputList[i + 5])
             secondInt = str(inputList[i + 7] + inputList[i + 8] + inputList[i + 9])
-            multipliedIntegers = int(firstInt) * int(secondInt)
-        # XXX, Y
+        # XXX,Y
         elif inputList[i + 7] == "," and inputList[i + 9] == ")":
             firstInt = str(inputList[i + 4] + inputList[i + 5] + inputList[i + 6])
-            multipliedIntegers = int(firstInt) * int(inputList[i + 8])
-        # XXX, YY
+            secondInt = inputList[i + 8]
+        # XXX,YY
         elif inputList[i + 7] == "," and inputList[i + 10] == ")":
             firstInt = str(inputList[i + 4] + inputList[i + 5] + inputList[i + 6])
             secondInt = str(inputList[i + 8] + inputList[i + 9])
-            multipliedIntegers = int(firstInt) * int(secondInt)
-        # XXX, YYY
-        elif inputList[i + 7] == "," and inputList[i + 11]:
+        # XXX,YYY
+        elif inputList[i + 7] == "," and inputList[i + 11] == ")":
             firstInt = str(inputList[i + 4] + inputList[i + 5] + inputList[i + 6])
             secondInt = str(inputList[i + 8] + inputList[i + 9] + inputList[i + 10])
-            multipliedIntegers = int(secondInt) * int(secondInt)
     
-    
+        
+        multipliedIntegers = int(firstInt) * int(secondInt)
         multipliedIntegerProducts.append(multipliedIntegers)
     else:
-        print("Wrong")
+        print(inputList[i])
         
         
 print(inputList)
